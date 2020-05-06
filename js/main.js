3f4ec449c8be298e532de455908388072dda84e2
harsh kumar <harshkumar2009@gmail.com>
2020-05-06 16:37:27 +0530

    intial comit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+    new WOW().init();
+
+
+    $(".burger-nav").on("click", function () {
+
+        $(".container nav ul").toggleClass("open");
+
+    });
+
+
+
+
+    
+
+
+
+
+});
